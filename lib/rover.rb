8f535fa276d8c7b9d0c399dd71de33a41581be8c
Caio Costa Salgado <caiosalgado@linux.ime.usp.br>
2016-04-29 17:25:08 -0300

    created rover class and initial tests

---
+++ lib/rover.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+class Rover
+	attr_accessor :position, :facing, :number_id
+
+  def spin(direction)
+  end
+  def forward
+  end
+  def get_position_with_facing
+  end  
+end
---
+++ spec/rover_spec.rb
@@ -0,0 +1,34 @@
+require 'rover'
+
+
+describe "Rover object" do
+	before :each do
+  	@rover = Rover.new [0,0] , "N", 1
+	end
+
+	context "#spin while facing north" do
+		it "change facing 90 degrees to the right" do
+			@rover.spin("R")
+			expect(@rover.facing).to eq "E"
+		end
+		it "change facing 90 degrees to the left" do
+			@rover.spin("L")
+			expect(@rover.facing).to eq "W"
+		end
+	end
+
+	context "#forward while facing north and at 0,0" do
+		it "change position +1 in y coordinate" do
+			@rover.forward
+			expect(@rover.position).to eq [0,1]
+		end
+	end
+
+	context "while facing north and at 0,0" do
+		it "should get me the right position" do
+			position = @rover.get_position_with_facing
+			expect(position).to eq [0,0,"N"]
+		end
+	end
+
+end
